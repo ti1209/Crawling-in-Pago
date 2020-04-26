d5f9fce0bc994d4f28557a86fb9b02092161cd17
ti1209 <ti1209@naver.com>
2020-04-26 18:13:57 +0900

    Update news_spider.py

--- news/spiders/news_spider.py
+++ news/spiders/news_spider.py
@@ -1,33 +1,28 @@
 import scrapy
 from ..items import NewsItem
-from selenium import webdriver
 
 
 class NewsUrlSpider(scrapy.Spider):
     name = 'newsUrl'
 
-    driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-    driver.implicitly_wait(3)
-    driver.get('')
-
-    # def start_requests(self):
-    #     yield scrapy.Request("https://www.boannews.com/media/o_list.asp", self.parse_url)
-    #
-    # def parse_url(self, response):
-    #     for news in response.xpath('//div[@id="news_area"]/div[@class="news_list"]'):
-    #         temp_url = ''.join(['https://www.boannews.com'] + news.xpath('a[2]/@href').extract())
-    #         request = scrapy.Request(temp_url, callback=self.parse_news)
-    #
-    #         yield request
-    #
-    # def parse_news(self, response):
-    #     item = NewsItem()
-    #
-    #     item['title'] = response.xpath('//*[@id="news_title02"]/text()').extract()
-    #     item['content'] = response.xpath('//*[@id="news_content"]/text()').extract()
-    #     item['date'] = response.xpath('//*[@id="news_util01"]/text()').extract()
-    #     item['url'] = response.xpath('/html/head/link[2]/@href').extract()
-    #
-    #     print(item['url'])
-    #
-    #     yield item
+    def start_requests(self):
+        yield scrapy.Request("https://www.boannews.com/media/o_list.asp", self.parse_url)
+    
+    def parse_url(self, response):
+        for news in response.xpath('//div[@id="news_area"]/div[@class="news_list"]'):
+            temp_url = ''.join(['https://www.boannews.com'] + news.xpath('a[2]/@href').extract())
+            request = scrapy.Request(temp_url, callback=self.parse_news)
+    
+            yield request
+    
+    def parse_news(self, response):
+        item = NewsItem()
+    
+        item['title'] = response.xpath('//*[@id="news_title02"]/text()').extract()
+        item['content'] = response.xpath('//*[@id="news_content"]/text()').extract()
+        item['date'] = response.xpath('//*[@id="news_util01"]/text()').extract()
+        item['url'] = response.xpath('/html/head/link[2]/@href').extract()
+    
+        print(item['url'])
+    
+        yield item
